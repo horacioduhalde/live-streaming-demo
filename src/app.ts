f520140646198f536ba5506324db4d5f350ae953
DePatas <depatasok@gmail.com>
2023-11-10 11:28:27 -0300

    initial commit 2

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,8 @@
  //Load environment variables (populate process.env from .env file)
  import * as dotenv from "dotenv";
 
+ import { ChatOpenAI } from "langchain/chat_models/openai";
+
  //dotenv.config();
  //console.log(process.env);
  const result = dotenv.config();
@@ -40,3 +42,25 @@
  };
 
  run();
+
+
+
+const model = new ChatOpenAI({});
+const promptTemplate = PromptTemplate.fromTemplate(
+  "Tell me a joke about {topic}"
+);
+
+const chain = promptTemplate.pipe(model);
+
+const stream = await chain.stream({ topic: "bears" });
+
+// Each chunk has the same interface as a chat message
+for await (const chunk of stream) {
+  console.log(chunk?.content);
+}
+
+/*
+Why don't bears wear shoes?
+
+Because they have bear feet!
+*/
